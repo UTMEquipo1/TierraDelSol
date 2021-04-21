1fcd22d0e49feed48d3579f36a623c679751ecec
Will-Mdez <82000613+Will-Mdez@users.noreply.github.com>
2021-04-21 17:25:00 -0500

    Add files via upload

---
+++ Inventario.java
@@ -0,0 +1,11 @@
+package persistencia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventario {
+	private String NombreTienda;
+    private List<Producto> ListaProductos= new ArrayList<Producto>();
+    private int ID;
+
+}
